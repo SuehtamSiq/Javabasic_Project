de454445a47794552e41c025029fa348d0ec38c0
Matheus Siqueira <m03.siqueira@gmail.com>
2025-03-26 18:51:26 -0300

    commit de tiposVariaveis

---
+++ ProjectJv/src/exeDio/sintaxeJva/tiposVariaveis/tiposVariaveis.java
@@ -0,0 +1,33 @@
+package exeDio.sintaxeJva.tiposVariaveis;
+
+public class tiposVariaveis {
+
+    public static void main(String[] args) throws Exception {
+        
+        /*  
+        * Tipos de variáveis presentes na linguagem,
+        * com exceção do tipo char.
+        */
+
+        byte age = 1;
+        short year = 2025;
+        int numberHome = 260;
+        long id = 65474837;
+        float price = 0;
+        double salary = 2500.80;
+        boolean sim = true;
+        String name = "Matheus";
+
+        /*
+         * Ao usar a palavra reservada final, você determina uma constante,
+         * ou seja, declara que esta variavel não podera receber outro valor;
+         * 
+         * Além de que todas as varaiveis final deven conter
+         * o nome em CAIXA ALTA.
+         */
+        
+        final double VALOR_DE_PI = 3.14;
+
+    }
+    
+}
