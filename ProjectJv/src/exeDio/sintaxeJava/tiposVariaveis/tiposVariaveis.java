f72ff15d96fe6eb17b6b3a8812bca6600fb1cb61
Matheus Siqueira <m03.siqueira@gmail.com>
2025-03-26 20:19:12 -0300

    commit operadores

--- ProjectJv/src/exeDio/sintaxeJava/tiposVariaveis/tiposVariaveis.java
+++ ProjectJv/src/exeDio/sintaxeJava/tiposVariaveis/tiposVariaveis.java
@@ -1,12 +1,11 @@
-package exeDio.sintaxeJva.tiposVariaveis;
+package exeDio.sintaxeJava.tiposVariaveis;
 
 public class tiposVariaveis {
 
     public static void main(String[] args) throws Exception {
         
         /*  
-        * Tipos de variáveis presentes na linguagem,
-        * com exceção do tipo char.
+        * Tipos de variáveis presentes na linguagem.
         */
 
         byte age = 1;
@@ -15,8 +14,21 @@ public class tiposVariaveis {
         long id = 65474837;
         float price = 0;
         double salary = 2500.80;
-        boolean sim = true;
+        boolean yes = true;
+        char sex = 'M';
         String name = "Matheus";
+
+        
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(numberHome);
+        System.out.println(id);
+        System.out.println(price);
+        System.out.println(salary);
+        System.out.println(yes);
+        System.out.println(sex);
+        System.out.println(name);
+        
 
         /*
          * Ao usar a palavra reservada final, você determina uma constante,
@@ -27,6 +39,8 @@ public class tiposVariaveis {
          */
         
         final double VALOR_DE_PI = 3.14;
+
+        System.out.println("O valor de PI é " + VALOR_DE_PI);
 
     }
     
