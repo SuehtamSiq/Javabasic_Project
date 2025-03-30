61e73dd66d0591967a46fb8e2e76ec3c3bc260ae
Matheus Siqueira <m03.siqueira@gmail.com>
2025-03-30 15:12:02 -0300

    commit metodos

---
+++ ProjectJv/src/exeDio/sintaxeJava/métodos/UsuarioTv.java
@@ -0,0 +1,32 @@
+package exeDio.sintaxeJava.métodos;
+
+public class UsuarioTv {
+    
+    public static void main(String[] args) throws Exception {
+        
+         métodos smartTv = new métodos();
+          
+         System.out.println("TV ligada? " + smartTv.ligada);
+         System.out.println("Canal atual: " + smartTv.canal);
+         System.out.println("Volume atual: " + smartTv.volume);
+
+         smartTv.ligar();
+         System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+         smartTv.aumentarVolume();
+         System.out.println("Volume atual: " + smartTv.volume);
+        
+         smartTv.dimonuirVolume();
+         System.out.println("Volume atual: " + smartTv.volume);
+        
+         smartTv.proximoCanal();
+         System.out.println("Canal atual: " + smartTv.canal);
+
+         smartTv.canalAnterior();
+         System.out.println("Canal atual: " + smartTv.canal);
+
+         smartTv.escolherCanal();
+         System.out.println("Canal atual: " + smartTv.canal);
+    }
+
+}
---
+++ ProjectJv/src/exeDio/sintaxeJava/métodos/métodos.java
@@ -0,0 +1,47 @@
+package exeDio.sintaxeJava.métodos;
+
+import java.util.Scanner;
+public class métodos {
+
+    // Sistema_SmartTv
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 30;
+    Scanner scanner = new Scanner(System.in); // Cria um objeto Scanner
+    
+
+    public void ligar(){
+        ligada=true;
+    } 
+
+    public void desligar(){
+        ligada=false;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+    }
+
+    public void dimonuirVolume(){
+        volume--;
+    }
+
+    public void proximoCanal(){
+        canal++;
+    }
+
+    public void canalAnterior(){
+        canal--;
+    }
+
+    public void escolherCanal(){
+       System.out.print("Qual canal desejado? ");
+       canal = scanner.nextInt();
+
+    }
+
+
+
+
+}
