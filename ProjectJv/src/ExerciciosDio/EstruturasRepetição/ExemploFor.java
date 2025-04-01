f99ace7aa42171a055caa3890f3484df43f9659d
Matheus Siqueira <m03.siqueira@gmail.com>
2025-04-01 15:38:27 -0300

    commit repetições

---
+++ ProjectJv/src/ExerciciosDio/EstruturasRepetição/ExemploFor.java
@@ -0,0 +1,14 @@
+package ExerciciosDio.EstruturasRepetição;
+
+public class ExemploFor {
+
+    public static void main(String[] args) {
+        
+        for (int carneirinhos = 0; carneirinhos <= 20; carneirinhos++){
+            System.out.println("Contando carneirinhos: " + carneirinhos);
+        }
+
+        System.out.println("Joãozinho dormiu!");;
+    }
+    
+}
