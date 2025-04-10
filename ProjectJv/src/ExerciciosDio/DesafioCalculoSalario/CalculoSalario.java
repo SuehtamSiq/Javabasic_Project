68bbd94501084d8600fde245a9d506388c9b1b9d
Matheus Siqueira <m03.siqueira@gmail.com>
2025-04-10 20:54:07 -0300

    commit project salario

---
+++ ProjectJv/src/ExerciciosDio/DesafioCalculoSalario/CalculoSalario.java
@@ -0,0 +1,33 @@
+package ExerciciosDio.DesafioCalculoSalario;
+
+import java.util.Scanner;
+
+public class CalculoSalario {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Qual é o seu salário: ");
+        double valorSalario = scanner.nextDouble();
+        System.out.print("Quanto em benefícios: ");
+        double valorBeneficios = scanner.nextDouble();
+
+
+        double valorImposto = 0.0;
+        if (valorSalario >= 0 && valorSalario <= 1100) {
+            valorImposto = 0.05f * valorSalario;
+        }
+        else if (valorSalario >=1100.01 && valorSalario <=2500) {
+            valorImposto = 0.10f * valorSalario;
+        }
+        else {
+            valorImposto = 0.15f * valorSalario;
+        }
+
+        double saida = valorSalario - valorImposto + valorBeneficios;
+        System.out.printf("%.2f", saida);
+
+        scanner.close();
+    }
+    
+}
