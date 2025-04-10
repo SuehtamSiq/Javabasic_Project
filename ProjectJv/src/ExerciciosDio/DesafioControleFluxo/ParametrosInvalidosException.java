2eeb60d8d0ed75b904abbf187662a1d0bc0082eb
Matheus Siqueira <m03.siqueira@gmail.com>
2025-04-09 22:41:55 -0300

    commit project dio

---
+++ ProjectJv/src/ExerciciosDio/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package ExerciciosDio.DesafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        System.out.println(mensagem);
+    }
+}
